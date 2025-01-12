cc051e92f7e648c084a55abdcf6095153eeaa8cd
kotecat <davidshaldunov@gmail.com>
2025-01-12 11:44:59 +0500

    Add "Open..." button in menu

--- VRoidHubLoader/Core.cs
+++ VRoidHubLoader/Core.cs
@@ -1,13 +1,14 @@
-﻿using System.IO;
-using System.Runtime.InteropServices;
-using CustomAvatarLoader;
+﻿using CustomAvatarLoader;
 using Il2Cpp;
+using Il2CppTMPro;
 using Il2CppUniGLTF;
 using Il2CppUniVRM10;
 using MelonLoader;
 using UnityEngine;
+using UnityEngine.InputSystem.Utilities;
+using UnityEngine.UI;
 
-[assembly: MelonInfo(typeof(Core), "Custom Avatar Loader Mod", "1.0.3", "SergioMarquina, Misandrie")]
+[assembly: MelonInfo(typeof(Core), "Custom Avatar Loader Mod", "1.0.3", "SergioMarquina, Misandrie, MrKoteee")]
 [assembly: MelonGame("infiniteloop", "DesktopMate")]
 
 namespace CustomAvatarLoader;
@@ -26,19 +27,24 @@ public class Core : MelonMod
     }
 
     private bool _init = false;
+    private bool _init_buttons = false;
 
     public override void OnUpdate()
     {
         if (Input.GetKeyDown(KeyCode.F4))
         {
-            string path = FileHelper.OpenFileDialog();
-            if (!string.IsNullOrEmpty(path) && LoadCharacter(path))
-            {
-                _vrmPath.Value = path;
-                _init = true;
-                MelonPreferences.Save();
-            }
+            ShowChooseModelWindow();
         }
+
+        // Add Buttons
+        if (!_init_buttons && GameObject.Find("iltanButton") is GameObject iltanButton)
+        {
+            _init_buttons = true;
+            Action actionChoose = new Action(() => ShowChooseModelWindow());
+            AddButton(iltanButton.transform.parent, actionChoose, "Open...");
+            
+        }
+        // End code add Buttons
 
         if (!_init && GameObject.Find("/CharactersRoot").transform.GetChild(0) != null)
         {
@@ -51,6 +57,66 @@ public class Core : MelonMod
         
         _vrmPath.Value = "";
         MelonPreferences.Save();
+    }
+
+    private void ShowChooseModelWindow()
+    {
+        string path = FileHelper.OpenFileDialog();
+        if (!string.IsNullOrEmpty(path) && LoadCharacter(path))
+        {
+            _vrmPath.Value = path;
+            _init = true;
+            MelonPreferences.Save();
+        }
+    }
+
+    private void RemoveCustomButtons(Transform parent)
+    {
+        var contentButtons = parent.gameObject;
+
+        for (int i = 0; i < contentButtons.transform.childCount; ++i)
+        {
+            var button = contentButtons.transform.GetChild(i).gameObject;
+            if (button.transform.childCount >= 0)
+            {
+                var textField = button.transform.GetChild(0).gameObject;
+                var textMeshComp = textField.GetComponent<TextMeshProUGUI>();
+                var text = textMeshComp.text;
+
+                if (textField.name.InvariantEqualsIgnoreCase("Text (Custom)"))
+                {
+                    button.active = false;
+                    UnityEngine.Object.Destroy(button);
+                    LoggerInstance.Msg($"Button [{text}]: Has been removed.");
+                }
+            }
+        }
+    }
+
+    private bool AddButton(Transform parent, Action action, string text)
+    {
+        var baseButton = GameObject.Find("MenuCanvas/MenuParent/RootPage/Scroll View/Viewport/Content/ModelButton");
+        if (baseButton == null) return false;
+
+        GameObject newButton = GameObject.Instantiate(baseButton, parent);
+        Image newButtonImage = newButton.GetComponent<Image>();
+        newButtonImage.sprite = null;
+
+        Button newButtonComp = newButton.GetComponent<Button>();
+        newButtonComp.onClick.RemoveAllListeners();
+        newButtonComp.onClick.AddListener(action);
+
+        var textField = newButton.transform.FindChild("Text (TMP)").gameObject;
+        textField.name = "Text (Custom)";
+        textField.active = true;
+
+        var textMeshComp = textField.GetComponent<TextMeshProUGUI>();
+        textMeshComp.text = text;
+        textMeshComp.GenerateTextMesh();
+
+        LoggerInstance.Msg($"Button [{text}]: Has been added.");
+
+        return true;
     }
 
     private bool LoadCharacter(string path)
