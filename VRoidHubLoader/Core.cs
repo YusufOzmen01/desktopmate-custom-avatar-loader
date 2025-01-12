2fcd44fbe4fb34702c6ef9074aa3d603975f0325
Chris <chris.hart@morphics.co.uk>
2025-01-12 15:38:30 +0000

    Moved CharaLoader into VrmLoaderModule

--- VRoidHubLoader/Core.cs
+++ VRoidHubLoader/Core.cs
@@ -1,26 +1,19 @@
-﻿using CustomAvatarLoader.Chara;
+﻿namespace CustomAvatarLoader;
+
+using CustomAvatarLoader.Chara;
 using CustomAvatarLoader.Helpers;
-
-namespace CustomAvatarLoader;
-
+using CustomAvatarLoader.Modules;
 using Logging;
-using Modules;
-using Versioning;
-using Il2Cpp;
-using Il2CppUniGLTF;
-using Il2CppUniVRM10;
 using MelonLoader;
-using System.Diagnostics;
-using System.Runtime.InteropServices;
-using UnityEngine;
+using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
+using UnityEngine;
+using Versioning;
 
 public class Core : MelonMod
 {
     private const string RepositoryName = "YusufOzmen01/desktopmate-custom-avatar-loader";
     private bool _init;
-
-    protected virtual IServiceProvider Services { get; }
     
     private Logging.ILogger Logger { get; set; }
 
@@ -28,35 +21,31 @@ public class Core : MelonMod
     
     private Updater Updater { get; set; }
     
-    private FileHelper FileHelper { get; set; }
-    private VrmLoader VrmLoader { get; set; }
-    
     private CharaLoader CharaLoader { get; set; }
 
     private string CurrentVersion { get; set; }
 
-    private MelonPreferences_Category Settings { get; set; }
+    protected IServiceProvider ServiceProvider { get; private set; }
 
-    private MelonPreferences_Entry<string> VrmPath { get; set; }
+    protected IEnumerable<IModule> Modules { get; private set; }
 
     public override void OnInitializeMelon()
     {
+        var services = new ServiceCollection();
+        ConfigureServices(services);
+        ServiceProvider = services.BuildServiceProvider();
+
+        Modules = ServiceProvider.GetServices<IModule>();
+
         CurrentVersion = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "0";
 
         Logger = new MelonLoaderLogger(LoggerInstance);
         VersionChecker = new GitHubVersionChecker(RepositoryName, Logger);
         Updater = new Updater(RepositoryName, Logger);
-        FileHelper = new FileHelper();
-        VrmLoader = new VrmLoader(Logger);
-        CharaLoader = new CharaLoader(Logger, VrmLoader);
 
         if (CurrentVersion == "0")
             Logger.Warn("CurrentVersion is 0, faulty module version?");
         
-        // Initialize your preferences
-        Settings = MelonPreferences.CreateCategory("settings");
-        VrmPath = Settings.CreateEntry("vrmPath", "");
-
         var hasLatestVersion = VersionChecker.IsLatestVersionInstalled(CurrentVersion);
 
         if (!hasLatestVersion)
@@ -67,31 +56,25 @@ public class Core : MelonMod
         {
             Logger.Info("[VersionCheck] Latest version installed");
         }
+
+        foreach (var module in Modules)
+        {
+            module.OnInitialize();
+        }
+    }
+
+    private void ConfigureServices(IServiceCollection services)
+    {
+        services.AddSingleton(typeof(MelonLogger.Instance), LoggerInstance);
+        services.AddScoped(typeof(Logging.ILogger), typeof(MelonLoaderLogger));
+        services.AddScoped(typeof(IModule), typeof(VrmLoaderModule));
     }
 
     public override void OnUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.F4))
+        foreach (var service in Modules)
         {
-            string path = FileHelper.OpenFileDialog();
-            if (!string.IsNullOrEmpty(path) && CharaLoader.LoadCharacter(path))
-            {
-                VrmPath.Value = path;
-                _init = true;
-                MelonPreferences.Save();
-            }
+            service.OnUpdate();
         }
-
-        if (!_init && GameObject.Find("/CharactersRoot").transform.GetChild(0) != null)
-        {
-            _init = true;
-            if (VrmPath.Value != "") CharaLoader.LoadCharacter(VrmPath.Value);
-        }
-
-        if (!_init || GameObject.Find("/CharactersRoot/VRMFILE") != null || VrmPath.Value == "")
-            return;
-
-        VrmPath.Value = "";
-        MelonPreferences.Save();
     }
 }
